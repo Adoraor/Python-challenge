c1e48bd7609912667d1ad128ae61af80d92dee46
Adora O <adora@Adoras-MacBook-Pro.local>
2023-05-18 23:50:14 -0400

    Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,24 +9,24 @@ with open(bank_csv) as csv_file:
 # Read header    
     csv_header = next(csv_file)
     
-    x =[]
-    y =[]
+    Dates =[]
+    Profit_Losses =[]
     
     for row in csv_reader:
         Date = row[0]
-        Profit_Losses = int(row[1])
-        x.append(Date)
-        y.append(Profit_Losses)
+        Profit_Loss = int(row[1])
+        Dates.append(Date)
+        Profit_Losses.append(Profit_Loss)
 # total number of months included in the dataset
-total_months = len(set(x))
+total_months = len(set(Dates))
         
 # net total amount of "Profit/Losses" over the entire period
-entire_total_amount = sum(y)
+entire_total_amount = sum(Profit_Losses)
 
 # changes in "Profit/Losses" over the entire period, and then the average of those changes, greatest increase and decrease
 
-changes_profit_loss = [y[i] - y[i - 1] 
-           for i in range(1, len(y))]
+changes_profit_loss = [Profit_Losses[i] - Profit_Losses[i - 1] 
+           for i in range(1, len(Profit_Losses))]
 average = sum(changes_profit_loss)/ len(changes_profit_loss)
 average_changes = round(average, 2)
 
@@ -36,19 +36,17 @@ greatest_decrease = min(changes_profit_loss)
 greatest_increase_index = changes_profit_loss.index(greatest_increase) + 1
 greatest_decrease_index = changes_profit_loss.index(greatest_decrease) + 1
 
-greatest_increase_date = x[greatest_increase_index]
-greatest_decrease_date = x[greatest_decrease_index]
+greatest_increase_date = Dates[greatest_increase_index]
+greatest_decrease_date = Dates[greatest_decrease_index]
 
-
-
-# final output-----------------------------------------------------------------
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {total_months}")
-print(f"Total: ${entire_total_amount}")
-print(f"Average Change: ${average_changes}")
-print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
-print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
+with open('/Users/adora/Desktop/BOOTCAMP_ASSIGNMENTS/Python_challenge/Python-challenge/PyBank/analyis/bank.txt', 'w') as f:
+    f.write('Financial Analysis\n')
+    f.write('----------------------------\n')
+    f.write(f'Total Months: {total_months}\n')
+    f.write(f'Total: ${entire_total_amount}\n')
+    f.write(f'Average Change: ${average_changes}\n')
+    f.write(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})\n')
+    f.write(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n')
 
    
    
